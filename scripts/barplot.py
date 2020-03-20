968d8806185256b6f76996389bdb3073f706af5a
utkarsh <utkarshgiri18@gmail.com>
2020-03-20 02:37:43 -0400

    .

---
+++ scripts/barplot.py
@@ -0,0 +1,39 @@
+import fire
+import numpy
+import pandas
+from covid import covid
+from scipy import stats
+from matplotlib import pyplot as plt
+#from palettable.tableau import Tableau_20_r
+
+frame = covid.get_entire_frame()
+frame.reset_index(level=0, inplace=True)
+frame.sort_values(by=frame.columns[-1], ascending=False, inplace=True)
+
+def compare(country='US', against='Italy', lastndays=12, plot=False):
+    #Inspired by https://twitter.com/KashifMD/status/1239396389085499393?s=20
+    country_one = frame[frame['Country/Region'] == country].values.squeeze()[1:][:]
+    country_one = country_one[country_one>20][-lastndays:]
+    country_two = frame[frame['Country/Region'] == against].values.squeeze()[:][1:]
+    
+    diff = country_two - country_one[0]
+    country_one = country_one[1:]
+    country_two = country_two[diff>0][:len(country_one)+4]
+    fig, ax = plt.subplots()
+    ax.bar(numpy.arange(len(country_two)), country_two, width=0.5, label=against)
+    ax.bar(numpy.arange(len(country_one))+0.5, country_one, width=0.5, label=country)
+    ax.legend()
+    plt.savefig('../plots/{}_vs_{}_barplot.pdf'.format(country, against))
+    plt.show()
+ 
+def solo(country='US',lastndays=12, plot=False):
+    country_one = frame[frame['Country/Region'] == country].values.squeeze()[1:][:]
+    country_one = country_one[country_one>20][-lastndays:]
+    fig, ax = plt.subplots()
+    ax.bar(numpy.arange(len(country_one)), country_one, label=country)
+    ax.legend()
+    plt.savefig('plots/{}_barplot.pdf'.format(country))
+    plt.show()
+    
+if __name__ == '__main__':
+    fire.Fire()
---
+++ scripts/growth.py
@@ -0,0 +1,71 @@
+import fire
+import numpy
+import pandas
+from covid import covid
+from scipy import stats
+from matplotlib import pyplot as plt
+#from palettable.tableau import Tableau_20_r
+
+frame = covid.get_entire_frame()
+tframe = pandas.read_csv('https://raw.githubusercontent.com/imantsm/COVID-19/master/csv/tMin.csv')
+frame.reset_index(level=0, inplace=True)
+frame.sort_values(by=frame.columns[-1], ascending=False, inplace=True)
+
+def country(name, lastndays=12, plot=False):
+    time = numpy.arange(lastndays)
+    cumulative = frame[frame['Country/Region'] == name].values.squeeze()[1:][-lastndays:].astype(numpy.float64)
+    slope, intercept, r, _, _ = stats.linregress(numpy.arange(lastndays), numpy.log(cumulative[-lastndays:]))
+    print('{}:\n\nGrowth rate : {},\nDoubling time: {} days,\nr : {}\n'.format(name, slope, numpy.log(2)/slope, r))
+    if plot:
+        plt.plot(numpy.arange(lastndays), numpy.log(cumulative[-lastndays:]), 'o')
+        plt.plot(numpy.arange(lastndays), slope*numpy.arange(lastndays) + intercept)
+        plt.savefig('plots/{}.pdf'.format(name))
+    return slope, r
+
+
+def median_age():
+    df = pandas.read_html('https://en.wikipedia.org/wiki/List_of_countries_by_median_age')
+    return df[0]
+
+
+def cleaned_country(name, cull_below=20):
+    cumulative = frame[frame['Country/Region'] == name].values.squeeze()[:][1:].astype(float)
+    cumulative = cumulative[cumulative>cull_below]
+    if len(cumulative) < 2:
+        return None, 0
+    time = numpy.arange(len(cumulative))
+    slope, intercept, r, _, _ = stats.linregress(time, numpy.log(cumulative))
+    return slope, r
+
+
+def minimum_temperature(name):
+    dataframe = tframe[tframe['Country/Region'].str.contains(name)]
+    dataframe.drop(dataframe.columns[[0, 1, 2, 3, 4, 5]], axis = 1, inplace = True)
+    return dataframe.transpose().tail(40).mean().values[0]
+
+
+def plot_growth_vs_temp(topn=50, r2_cutoff=0.96):
+    countries = frame['Country/Region'][:topn]
+    fig, ax = plt.subplots(dpi=350)
+    #ax.set_prop_cycle('color', Tableau_20_r.mpl_colors)
+    marker = numpy.array([numpy.repeat(x, 10) for x in ['o', '*', 'v', 's', 'd']]).flatten()
+    for i,name in enumerate(countries):
+        growth, r = cleaned_country(name)
+        if r**2 > r2_cutoff:
+            mint = minimum_temperature(name)
+            ax.plot(mint, growth, marker[i], label='{} [$r^2={:.2f}$]'.format(name, r**2))
+        else:
+            continue
+    ax.legend(bbox_to_anchor=(1.3, 0.9), fontsize=4.5)
+    plt.ylabel('Growth Rate')
+    plt.xlabel(r'Average  Minimum Temperature ($^o C$)')
+    plt.grid(ls='--')
+    plt.tight_layout()
+    plt.savefig('../plots/growth_rate_vs_temperature.pdf')
+
+def mortality():
+    recovered = pandas.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv')
+
+
+if __name__ == '__main__':
+    fire.Fire()
